09a052ac455e10c82385b0f14279277038a250e6
Chii Yuen <chii.yuen@hotmail.com>
2023-12-02 23:06:59 +0800

    Add answers

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,34 +3,62 @@ import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val filePath = "death_state.csv"
-
     val source = Source.fromFile("src/main/deaths_state.csv")
 
     val data = source.getLines()
       .map(_.split(",").map(_.trim).toList)
       .toList
+      .tail
 
-    val dataMap = new HashMap[String, Map[String, Int]]
-    val dataRows = data.tail
+    val dataMap = extractData[String, Int](data, (x: String) => x, (x: String) => x.toInt)
 
-    for (row <- dataRows) {
+    source.close()
+
+    val answer1 = dataMap
+      .flatMap(_._2)
+      .groupBy(_._1)
+      .view.mapValues(_.map(_._2).sum)
+      .maxBy(_._2)
+      ._1
+
+    val answer2 = dataMap
+      .flatMap(_._2)
+      .groupBy(_._1)
+      .view.mapValues(_.map(_._2).sum)
+      .values
+      .sum
+      .toDouble / dataMap.size
+
+    val answer3 = dataMap
+      .flatMap(_._2)
+      .groupBy(_._1)
+      .view.mapValues(_.map(_._2).sum)
+      .toList
+      .sortBy(_._2)
+      .take(5)
+      .map(_._1)
+      .mkString(", ")
+
+    println("Which states in Malaysia have the highest deaths due to Covid?")
+    println("Answer: " + answer1)
+    println("\nWhat is the average death per day for Malaysia in the provided data?")
+    println("Answer: " + answer2)
+    println("\nWhich of the five states have the lowest death in the provided data?")
+    println("Answer: " + answer3)
+  }
+
+  def extractData[A, B](data: List[List[String]], convertState: String => A, convertCount: String => B): HashMap[String, Map[A, B]] = {
+    val dataMap = new HashMap[String, Map[A, B]]()
+
+    for (row <- data) {
       val date = row(0)
-      val state = row(1)
-      val deathCount = row(2).toInt
+      val state = convertState(row(1))
+      val deathCount = convertCount(row(5))
 
       val innerMap = dataMap.getOrElseUpdate(date, Map())
 
       innerMap.put(state, deathCount)
     }
-
-    source.close()
-
-    println("Which states in Malaysia have the highest deaths due to Covid?")
-    println(answer1)
-    println("What is the average death per day for Malaysia in the provided data?")
-    println(answer2)
-    println("Which of the five states have the lowest death in the provided data?")
-    println(answer3)
+    dataMap
   }
 }
